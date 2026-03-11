6b65f5e90093d83d34bfcbad4f0cd81c45ef7305
Harry Thomson <haroldt95@hotmail.com>
2026-03-11 15:24:26 +0900

    Automatically prepend and async driver to the DB

--- app/core/database.py
+++ app/core/database.py
@@ -3,6 +3,9 @@ from sqlalchemy.orm import DeclarativeBase
 
 from app.core.config import DATABASE_URL
 
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
+
 engine = create_async_engine(DATABASE_URL)
 
 SessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
