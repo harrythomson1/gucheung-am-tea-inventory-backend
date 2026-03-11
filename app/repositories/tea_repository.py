d438036d9d1b1cbc0c0a4e59050132cd7a957434
Harry Thomson <haroldt95@hotmail.com>
2026-03-11 10:43:35 +0900

    Setup repository

---
+++ app/repositories/tea_repository.py
@@ -0,0 +1,11 @@
+from sqlalchemy import select
+from app.models.tea import Tea
+
+class TeaRepository:
+    def __init__(self, db):
+        self.db = db
+
+    async def get_all(self):
+        query = select(Tea)
+        result = await self.db.execute(query)
+        return result.scalars().all()
