00403ecbff592044addecad0c511eace39c67e0e
Harry Thomson <haroldt95@hotmail.com>
2026-03-12 15:36:05 +0900

    Create method in service to get stock summary by a tea id

--- app/services/tea_service.py
+++ app/services/tea_service.py
@@ -1,6 +1,7 @@
+from app.enums import FlushType, PackagingType
 from app.models import Tea
 from app.repositories import TeaRepository
-from app.schemas import DashboardTeaResponse
+from app.schemas import DashboardTeaResponse, TeaVariantStockResponse
 
 
 class TeaService:
@@ -15,3 +16,14 @@ class TeaService:
 
     async def get_stock_summary(self) -> list[DashboardTeaResponse]:
         return await self.repository.get_stock_summary()
+
+    async def get_stock_summary_by_id(
+        self,
+        tea_id: int,
+        packaging: PackagingType | None = None,
+        flush: FlushType | None = None,
+        harvest_year: int | None = None,
+    ) -> list[TeaVariantStockResponse]:
+        return await self.repository.get_stock_summary_by_id(
+            tea_id, packaging, flush, harvest_year
+        )
