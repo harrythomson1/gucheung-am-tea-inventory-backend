99e7277d6d9209606f76fb9a0d238f690e777805
Harry Thomson <haroldt95@hotmail.com>
2026-03-12 14:21:09 +0900

    Add type hints to tea repository

--- app/repositories/tea_repository.py
+++ app/repositories/tea_repository.py
@@ -8,12 +8,12 @@ class TeaRepository:
     def __init__(self, db):
         self.db = db
 
-    async def get_all(self):
+    async def get_all(self) -> list[Tea]:
         query = select(Tea)
         result = await self.db.execute(query)
         return result.scalars().all()
 
-    async def get_by_id(self, id):
+    async def get_by_id(self, id: int) -> Tea:
         query = select(Tea).where(Tea.id == id).options(selectinload(Tea.tea_variants))
         result = await self.db.execute(query)
         return result.scalars().first()
