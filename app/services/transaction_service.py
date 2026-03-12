a6d15839f63a56434914a20f11effa3af4e110ca
Harry Thomson <haroldt95@hotmail.com>
2026-03-12 11:03:18 +0900

    Start to build private _create_harvest method

--- app/services/transaction_service.py
+++ app/services/transaction_service.py
@@ -1,6 +1,13 @@
-class TransactionService:
-    def __init__(self, repository):
-        self.repository = repository
+from fastapi import HTTPException
 
-    async def create(self, transaction_info):
-        pass
+
+class TransactionService:
+    def __init__(self, repository, tea_repository, transaction_repository):
+        self.repository = repository
+        self.tea_repository = tea_repository
+        self.transaction_repository = transaction_repository
+
+    async def _create_harvest(self, transaction_info):
+        tea = self.tea_repository.get_by_id(transaction_info.tea_id)
+        if not tea:
+            raise HTTPException(status_code=404, detail="Tea not found")
