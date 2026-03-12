7c89005908f01ae2bcb2614fa7ba2286bce4c0d1
Harry Thomson <haroldt95@hotmail.com>
2026-03-12 14:35:18 +0900

    Create get stock summary method in the service

--- app/services/tea_service.py
+++ app/services/tea_service.py
@@ -1,5 +1,6 @@
 from app.models import Tea
 from app.repositories import TeaRepository
+from app.schemas import DashboardTeaResponse
 
 
 class TeaService:
@@ -11,3 +12,6 @@ class TeaService:
 
     async def get_by_id(self, id: int) -> Tea | None:
         return await self.repository.get_by_id(id)
+
+    async def get_stock_summary(self) -> list[DashboardTeaResponse]:
+        return await self.repository.get_stock_summary()
