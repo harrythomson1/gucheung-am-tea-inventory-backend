78e2ac5f0486d08f4a898d4ac59294c53b372105
Harry Thomson <haroldt95@hotmail.com>
2026-03-12 14:31:27 +0900

    Create a method to get all stock values for tea

--- app/repositories/tea_repository.py
+++ app/repositories/tea_repository.py
@@ -1,7 +1,8 @@
-from sqlalchemy import select
+from sqlalchemy import func, select
 from sqlalchemy.orm import selectinload
 
-from app.models.tea import Tea
+from app.models import StockTransaction, Tea, TeaVariant
+from app.schemas import DashboardTeaResponse
 
 
 class TeaRepository:
@@ -17,3 +18,17 @@ class TeaRepository:
         query = select(Tea).where(Tea.id == id).options(selectinload(Tea.tea_variants))
         result = await self.db.execute(query)
         return result.scalars().first()
+
+    async def get_stock_summary(self) -> list[DashboardTeaResponse]:
+        query = (
+            select(
+                Tea.id,
+                Tea.name,
+                func.sum(StockTransaction.quantity_change).label("total_stock"),
+            )
+            .join(TeaVariant, TeaVariant.tea_id == Tea.id)
+            .join(StockTransaction, StockTransaction.tea_variant_id == TeaVariant.id)
+            .group_by(Tea.id, Tea.name)
+        )
+        result = await self.db.execute(query)
+        return result.mappings().all()
