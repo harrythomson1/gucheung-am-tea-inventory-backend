f045f3bb5df8d92b6eda0574fdc1ffddd4ad9a59
Harry Thomson <haroldt95@hotmail.com>
2026-03-12 13:43:42 +0900

    Add dependencies needed for transaction service

--- app/dependencies.py
+++ app/dependencies.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.database import get_db
 from app.repositories.tea_repository import TeaRepository
+from app.repositories.tea_variant_repository import TeaVariantRepository
 from app.repositories.transaction_repository import TransactionRepository
 from app.services.tea_service import TeaService
 from app.services.transaction_service import TransactionService
@@ -16,6 +17,10 @@ async def get_transaction_repository(db: AsyncSession = Depends(get_db)):
     return TransactionRepository(db)
 
 
+async def get_tea_variant_repository(db: AsyncSession = Depends(get_db)):
+    return TeaVariantRepository(db)
+
+
 async def get_teas_service(
     tea_repository=Depends(get_tea_repository),
 ):
@@ -24,5 +29,11 @@ async def get_teas_service(
 
 async def get_transaction_service(
     transaction_repository=Depends(get_transaction_repository),
+    tea_repository=Depends(get_tea_repository),
+    tea_variant_repository=Depends(get_tea_variant_repository),
 ):
-    return TransactionService(repository=transaction_repository)
+    return TransactionService(
+        repository=transaction_repository,
+        tea_repository=tea_repository,
+        tea_variant_repository=tea_variant_repository,
+    )
