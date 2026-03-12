d17cb5dd9fa032b2c8d3772ebbaa6712c0952e94
Harry Thomson <haroldt95@hotmail.com>
2026-03-12 14:33:26 +0900

    Add type hints

--- app/services/tea_service.py
+++ app/services/tea_service.py
@@ -1,10 +1,13 @@
-class TeaService:
+from app.models import Tea
+from app.repositories import TeaRepository
 
-    def __init__(self, repository):
+
+class TeaService:
+    def __init__(self, repository: TeaRepository):
         self.repository = repository
 
-    async def get_all(self):
+    async def get_all(self) -> list[Tea]:
         return await self.repository.get_all()
 
-    async def get_by_id(self, id):
+    async def get_by_id(self, id: int) -> Tea | None:
         return await self.repository.get_by_id(id)
