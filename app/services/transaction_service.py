2fd91d36d952c90a9f6eb1ec5df0b1457b78dec6
Harry Thomson <haroldt95@hotmail.com>
2026-03-12 13:04:38 +0900

    Create _create_harvest method

--- app/services/transaction_service.py
+++ app/services/transaction_service.py
@@ -1,13 +1,27 @@
 from fastapi import HTTPException
 
+from app.models import StockTransaction
+from app.repositories import TeaRepository, TeaVariantRepository, TransactionRepository
+from app.schemas import CreateTransactionRequest
+
 
 class TransactionService:
-    def __init__(self, repository, tea_repository, transaction_repository):
+    def __init__(
+        self,
+        repository: TransactionRepository,
+        tea_repository: TeaRepository,
+        tea_variant_repository: TeaVariantRepository,
+    ):
         self.repository = repository
         self.tea_repository = tea_repository
-        self.transaction_repository = transaction_repository
+        self.tea_variant_repository = tea_variant_repository
 
-    async def _create_harvest(self, transaction_info):
-        tea = self.tea_repository.get_by_id(transaction_info.tea_id)
+    async def _create_harvest(
+        self, transaction_info: CreateTransactionRequest
+    ) -> StockTransaction:
+        tea = await self.tea_repository.get_by_id(transaction_info.tea_id)
         if not tea:
             raise HTTPException(status_code=404, detail="Tea not found")
+        variant = await self.tea_variant_repository.find_or_create(transaction_info)
+        transaction = await self.repository.create(transaction_info, int(variant.id))  # type: ignore
+        return transaction
