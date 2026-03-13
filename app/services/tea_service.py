a21b7022859acb0419652331e8433967767e6f7f
Harry Thomson <haroldt95@hotmail.com>
2026-03-13 15:25:29 +0900

    Create add method in tea_service

--- app/services/tea_service.py
+++ app/services/tea_service.py
@@ -1,7 +1,7 @@
 from app.enums import FlushType, PackagingType
 from app.models import Tea
 from app.repositories import TeaRepository
-from app.schemas import DashboardTeaResponse, TeaVariantStockResponse
+from app.schemas import CreateTeaRequest, DashboardTeaResponse, TeaVariantStockResponse
 
 
 class TeaService:
@@ -27,3 +27,6 @@ class TeaService:
         return await self.repository.get_stock_summary_by_id(
             tea_id, packaging, flush, harvest_year
         )
+
+    async def add(self, tea_info: CreateTeaRequest) -> Tea:
+        return await self.repository.add(tea_info=tea_info)
