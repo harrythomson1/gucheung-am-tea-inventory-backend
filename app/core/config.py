8b95ea372a335e334f3d94aace397085e336083f
Harry Thomson <haroldt95@hotmail.com>
2026-03-13 14:42:01 +0900

    Pull env variables from the .env file

--- app/core/config.py
+++ app/core/config.py
@@ -6,3 +6,11 @@ load_dotenv()
 
 DATABASE_URL: str = os.getenv("DATABASE_URL", "")
 assert DATABASE_URL, "DATABASE_URL is not set in .env"
+JWKS_URL: str = os.getenv("JWKS_URL", "")
+assert JWKS_URL, "JWKS_URL is not set in .env"
+PROJECT_URL: str = os.getenv("PROJECT_URL", "")
+assert PROJECT_URL, "PROJECT_URL is not set in .env"
+AUDIENCE: str = os.getenv("AUDIENCE", "")
+assert AUDIENCE, "AUDIENCE is not set in .env"
+ALGORITHM: str = os.getenv("ALGORITHM", "")
+assert ALGORITHM, "ALGORITHM is not set in .env"
