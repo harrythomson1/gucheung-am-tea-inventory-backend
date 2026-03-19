c08c8ba157b5bca1d5b3912920719b3893f6f44b
Harry Thomson <haroldt95@hotmail.com>
2026-03-19 10:21:04 +0900

    Set reponse on get_all method in customer respository

--- app/repositories/customer_repository.py
+++ app/repositories/customer_repository.py
@@ -1,13 +1,14 @@
 from sqlalchemy import select
 
-from app.models.customer import Customer
+from app.models import Customer
+from app.schemas import CustomerResponse
 
 
 class CustomerRepository:
     def __init__(self, db):
         self.db = db
 
-    async def get_all(self, skip: int = 0, limit: int = 20):
+    async def get_all(self, skip: int = 0, limit: int = 20) -> list[CustomerResponse]:
         query = select(Customer).offset(skip).limit(limit)
         result = await self.db.execute(query)
         return result.scalars().all()
