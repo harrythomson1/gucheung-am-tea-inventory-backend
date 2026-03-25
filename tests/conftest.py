6278073afe0602c569135d23d57684ec6f374858
Harry Thomson <haroldt95@hotmail.com>
2026-03-25 16:12:27 +0900

    Setup conftest to give a different event loop per test

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,23 +12,23 @@ TEST_DATABASE_URL = DATABASE_URL.replace(
     "gucheung_am_inventory", "gucheung_am_inventory_test"
 )
 
-engine = create_async_engine(TEST_DATABASE_URL)
-TestSessionLocal = async_sessionmaker(
-    engine, class_=AsyncSession, expire_on_commit=False
-)
 
-
-@pytest_asyncio.fixture(autouse=True)
-async def setup_database():
+@pytest_asyncio.fixture
+async def db_engine():
+    engine = create_async_engine(TEST_DATABASE_URL)
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    yield
+    yield engine
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
+    await engine.dispose()
 
 
 @pytest_asyncio.fixture
-async def db_session():
+async def db_session(db_engine):
+    TestSessionLocal = async_sessionmaker(
+        db_engine, class_=AsyncSession, expire_on_commit=False
+    )
     async with TestSessionLocal() as session:
         yield session
         await session.rollback()
